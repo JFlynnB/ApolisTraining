1d2911aa4045022553b485fa964ae79fb678bc4b
Flynn <ender753@gmail.com>
2025-04-28 22:12:22 -0400

    Basic running version

---
+++ KafkaPractice/JSPFront/src/main/java/com/example/JSPFront/JspFrontApplication.java
@@ -0,0 +1,15 @@
+package com.example.JSPFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JspFrontApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JspFrontApplication.class, args);
+	}
+
+}
